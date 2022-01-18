eef9d0c9add786900edc9c8b357b31ca391a2f3e
Antonio Niño Díaz <antonio_nd@outlook.com>
2022-01-18 00:10:36 +0000

    examples: Fix two broken tests
    Due to the changes to the code, they take now a bit longer to run.

--- examples/dma/hblank_dma_repeat/test.lua
+++ examples/dma/hblank_dma_repeat/test.lua
@@ -1,6 +1,6 @@
--- Test that runs a program for 10 frames, takes a screenshot and exits.
+-- Test that runs a program for 20 frames, takes a screenshot and exits.
 
-run_frames_and_pause(10)
+run_frames_and_pause(20)
 screenshot()
 exit()
 
